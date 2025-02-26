7b0014c00a016840d35f93e381b8767f9a639a09
adjones-aops <ajones@aops.com>
2025-02-25 16:30:24 -0800

    Add a crack.mp3 sound effect when the cookie is cracked

--- src/components/CookieAnimation.tsx
+++ src/components/CookieAnimation.tsx
@@ -1,7 +1,14 @@
 // src/components/CookieAnimation.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Howl } from 'howler';
 
 export default function CookieAnimation() {
+  useEffect(() => {
+    const sound = new Howl({
+      src: ['/sounds/crack.mp3'],
+    });
+    sound.play();
+  }, []);
   return (
     <div className="flex flex-col items-center space-y-2" data-testid="cookie-animation">
       {/* Placeholder for an animation - e.g., text or a static icon */}
